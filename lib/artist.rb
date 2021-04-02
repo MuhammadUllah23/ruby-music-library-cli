81f534e0aed682a265cb63838359aab669d88361
Muhammad <muhammadhullah23@gmail.com>
2021-04-02 18:01:26 -0400

    commit tree 2a311261b8af5240f96edd913217a4958201c9a3

---
+++ lib/artist.rb
@@ -0,0 +1,54 @@
+require 'pry'
+class Artist
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+    
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        @@all.clear
+    end
+
+    def self.create(artist)
+        artist = self.new(artist)
+        artist.save
+        artist
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def add_song(song)
+        if song.artist == nil
+            song.artist = self
+        else 
+            nil
+        end
+        if @songs.include?(song)
+            nil
+        else
+            @songs << song
+        end
+        song
+    end
+
+
+
+    
+end
---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song
+
+    attr_accessor :name, :artist
+
+    @@all = []
+
+    def initialize(name, artist=nil)
+        @name = name
+        self.artist=(artist) if artist != nil
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        @@all.clear
+    end
+
+    def self.create(song)
+        song = self.new(song)
+        song.save
+        song
+    end
+
+    
+
+end
